ad98423f16e678be6b08e3c148c67a63a5e453f4
Ruturajm4 <ruturaj.m4@gmail.com>
2024-07-16 12:54:26 +0530

    changes

--- src/DisplayName.jsx
+++ src/DisplayName.jsx
@@ -1,47 +1,52 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
-const DisplayName = ()=>{
+const DisplayName = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [display, setDisplay] = useState(false);
 
-    const [firstName, setFirstName]=useState()
-    const [lastName, setLastName]=useState()
+    const handleFirstName = (e) => {
+        setFirstName(e.target.value);
+    };
 
-    const [display, setDisplay]=useState(false)
-
-    const handleFirstName=(e)=>{
-        setFirstName(e.target.value)
-    }
-
-    const handleLastName=(e)=>{
-        setLastName(e.target.value)
-    }
-
-    // const show=()=>{
-        
-    // }
+    const handleLastName = (e) => {
+        setLastName(e.target.value);
+    };
 
     const handleSubmit = (event) => {
         event.preventDefault(); // Prevent default form submission
-        setDisplay((prev)=>!prev)
-    
+        setDisplay(true); // Set display to true on form submission
     };
 
-    return(
+    return (
         <div>
             <div>
-            <form onSubmit={handleSubmit}>
-                <h3>Full Name Display</h3>
-
-                <h6>First Name : <input type="text" onChange={handleFirstName} required="required"></input></h6> 
-                <h6>Last Name :  <input type="text" onChange={handleLastName} required="required"></input></h6>
-
-                <button type='submit'>Submit</button>
-
-                
-            </form>
+                <form onSubmit={handleSubmit}>
+                    <h3>Full Name Display</h3>
+                    <h6>
+                        First Name:{' '}
+                        <input
+                            type="text"
+                            onChange={handleFirstName}
+                            required="required"
+                            aria-label="First Name"
+                        />
+                    </h6>
+                    <h6>
+                        Last Name:{' '}
+                        <input
+                            type="text"
+                            onChange={handleLastName}
+                            required="required"
+                            aria-label="Last Name"
+                        />
+                    </h6>
+                    <button type="submit">Submit</button>
+                </form>
             </div>
-            <p>Full Name : {display && `${firstName} ${lastName}`}</p>
+            <p>Full Name: {display && `${firstName} ${lastName}`}</p>
         </div>
-    )
-}
+    );
+};
 
-export default DisplayName
+export default DisplayName;
